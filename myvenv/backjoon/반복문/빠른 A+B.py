7bd90791ddb6228e8bfa03e9eba1fe21d2b7321d
ohsangsoo <tkdtn3264@gmail.com>
2023-02-18 23:00:18 +0900

    백준 A+B - 7 11021 번 문제 풀이

--- myvenv/backjoon/반복문/빠른 A+B.py
+++ myvenv/backjoon/반복문/빠른 A+B.py
@@ -1,7 +1,8 @@
 import sys
-
+count_num=0
 count = int(input())
 for i in range(count):
     num1,num2 = map(int, sys.stdin.readline().split())
-    print(num1+num2)
-# 입출력 방식 input 이 느리면 여러 줄을 입력받거나 출력할 때 시간초과가 날 수 있다는 점이다. 그래서 sys.stdin.readine()쓰면 된다.
+    count_num+=1
+    print(f"Case #{count_num}: {num1+num2}")
+# # 입출력 방식 input 이 느리면 여러 줄을 입력받거나 출력할 때 시간초과가 날 수 있다는 점이다. 그래서 sys.stdin.readine()쓰면 된다.
